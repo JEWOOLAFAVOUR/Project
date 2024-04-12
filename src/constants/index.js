e4569863bf3b630e0648bf9e63df7fe2882c0df1
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-04-12 14:44:40 +0100

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,11 @@
+import icons from "./icons";
+import images from "./images";
+import appTheme, { COLORS, SIZES, FONTS } from "./theme";
+
+export {
+    icons,
+    images,
+    COLORS,
+    SIZES,
+    FONTS,
+}
